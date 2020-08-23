c7eb52a2cefc685d6738b3a9dad6d2e4f132c9b5
Stuart Olivera <stuart@stuartolivera.com>
2020-08-23 18:34:28 +0000

    Add plugin name to platform registration

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import { API } from 'homebridge';
 
-import { PLATFORM_NAME } from './settings';
+import { PLATFORM_NAME, PLUGIN_NAME } from './settings';
 import { ExampleHomebridgePlatform } from './platform'; 
 
 /**
  * This method registers the platform with Homebridge
  */
 export = (api: API) => {
-  api.registerPlatform(PLATFORM_NAME, ExampleHomebridgePlatform);
+  api.registerPlatform(PLUGIN_NAME, PLATFORM_NAME, ExampleHomebridgePlatform);
 }
